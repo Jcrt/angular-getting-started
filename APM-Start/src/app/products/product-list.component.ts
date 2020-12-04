fa5a5708974591045e907f2c83ee5a42a0c6c8b7
Julio Trindade <Julio.Trindade@thomsonreuters.com>
2020-12-03 21:29:36 -0300

    Creating service class

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from '../services/product.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'pm-products',
@@ -7,13 +9,6 @@ import { IProduct } from './product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  pageTitle = 'Product List';
-  imageWidth: number;
-  imageMargin: number;
-  showImage: boolean;
-
-  _listFilter: string;
-  filteredProducts: IProduct[];
 
   get listFilter(): string {
     return this._listFilter;
@@ -24,6 +19,25 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.performFilter(value);
   }
 
+  constructor(
+    private productService: ProductService
+  ) {
+    this.imageMargin = 2;
+    this.imageWidth = 50;
+    this.showImage = false;
+  }
+
+  _listFilter: string;
+
+  pageTitle = 'Product List';
+  imageWidth: number;
+  imageMargin: number;
+  showImage: boolean;
+
+  filteredProducts: IProduct[];
+
+  products: IProduct[];
+
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter(
@@ -32,40 +46,9 @@ export class ProductListComponent implements OnInit {
     );
   }
 
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2019",
-      "description": 'Curved claw steel hammer',
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
-
-  constructor() {
-    this.imageMargin = 2;
-    this.imageWidth = 50;
-    this.showImage = false;
-    this.listFilter = 'cart';
-
-    this.filteredProducts = this.products;
-  }
-
   ngOnInit(): void {
-    console.log('oie');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   toggleImage(): void {
